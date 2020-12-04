96125c05b84533e6b7e1f398f3040786d679fd89
Jordy Libbrecht <jordy.libbrecht@kbc.be>
2020-12-04 09:43:47 +0100

    Lab 8

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/config/ShoppingListConfig.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/config/ShoppingListConfig.java
@@ -1,5 +1,9 @@
 package com.ezgroceries.shoppinglist.config;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -17,5 +21,15 @@ public class ShoppingListConfig {
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort) {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
     }
 }
